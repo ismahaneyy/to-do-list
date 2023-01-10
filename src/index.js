4a55ac8098e19bdacc3ba5091d4f3c159da5929b
ismahaneyy <liciousmayrah@gmail.com>
2023-01-10 15:27:47 +0300

    Created ismahan-dev branch

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// declaring the variable 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
